ae0cb345352e2e1f545d81b4a764a77309eaecce
ZiadElme <s1380163@monmouth.edu>
2024-09-30 12:31:28 -0400

    This is version 2 of my hello world program

---
+++ helloWorld2.py
@@ -0,0 +1,9 @@
+Python 3.12.6 (v3.12.6:a4a2d2b0d85, Sep  6 2024, 16:08:03) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /Users/ziadelme/Documents/helloWorld.py ===============
+Hello my name is [Ziad]
+>>> 
+=============== RESTART: /Users/ziadelme/Documents/helloWorld.py ===============
+Hello my name is [Ziad]
+Hello my name is [Ziad] [Elmenshawy]
